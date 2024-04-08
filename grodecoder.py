d23726d0a34df986c0f3441cc70035c26ff67777
KarinDuong <karine@cacao.lbt.ibpc.fr>
2024-04-08 17:50:31 +0200

    add docstring for each function

--- grodecoder.py
+++ grodecoder.py
@@ -19,7 +19,7 @@ import networkx as nx
 from networkx.algorithms.components.connected import connected_components
 import matplotlib.pyplot as plt
 from loguru import logger
-from itertools import groupby 
+from itertools import groupby
 
 
 BOND_LENGTH = {'C-C': 1.54,
@@ -49,20 +49,24 @@ def matrice(file_gro):
     https://userguide.mdanalysis.org/stable/units.html#table-baseunits
     https://manual.gromacs.org/current/reference-manual/file-formats.html#gro )
 
-    Args:
-        file_gro (MDAnalysis.core.universe.Universe): An object
-            representing the molecular structure loaded from a GRO file.
+    Parameters
+    ----------
+        file_gro : MDAnalysis.core.universe.Universe
+            An object representing the molecular structure loaded from a GRO file.
 
-    Returns:
-        numpy.ndarray : matrix of interatomic distances between all atoms
+    Returns
+    -------
+        numpy.ndarray
+            matrix of interatomic distances between all atoms
     """
     position = file_gro.atoms.positions
     taille = len(file_gro.atoms)
     tmp = np.full((taille, taille), 100.0)
-    for i, pos_i in enumerate(position):
-        for j, pos_j in enumerate(position[i+1:], start=i+1):
-            tmp[i][j] = cdist([pos_i], [pos_j])
+    for index_i, pos_i in enumerate(position):
+        for index_j, pos_j in enumerate(position[index_i+1:], start=index_j+1):
+            tmp[index_i][index_j] = cdist([pos_i], [pos_j])
     return tmp
+
 
 def convert_atompairs_to_graph(atom_pairs, number_of_atoms):
     """Convert a list of pairs to a graph and its connected components.
@@ -73,8 +77,8 @@ def convert_atompairs_to_graph(atom_pairs, number_of_atoms):
     ----------
         atom_pairs : list
             A list of pairs representing edges in the graph.
-        number_of_atoms : 
-            
+        number_of_atoms : int
+            The total number of atoms in the graph.
 
     Returns
     -------
@@ -89,7 +93,7 @@ def convert_atompairs_to_graph(atom_pairs, number_of_atoms):
     return (nx.connected_components(graph), graph)
 
 
-def create_atompair(matrix_contact) : 
+def create_atompair(matrix_contact):
     """Create a list of atom pairs based on the contact matrix.
 
     This function takes a contact matrix as input and returns a list of atom pairs
@@ -114,17 +118,24 @@ def create_atompair(matrix_contact) :
 def relabel_node(graph, G, atom_names, resnames):
     """Iterate each connected subgraph, and relabel their node by [atom_name]_[index].
 
-    Args:
-        graph (generator): An object that lists all connected graphs
-        G (networkx.classes.graph.Graph): The graph for this file
-        atom_name (numpy.ndarray): A list of each atoms for this file
-        resnames ():  
+    Parameters
+    ----------
+        graph : generator
+            An object that lists all connected graphs
+        G : networkx.classes.graph.Graph
+            The graph for this file
+        atom_name : numpy.ndarray
+            A list of each atoms for this file
+        resnames : numpy.ndarray
+            A list of residue names for each node.
 
-    Returns:
-        list: list where each node is relabel
+    Returns
+    -------
+        list
+            list where each node is relabel
     """
     modified_subgraphs = []   # List to store modified subgraphs
-    
+
     # For each connected component, create a subgraph and add it to the list
     for component in graph:
         # Create a subgraph from the component
@@ -132,58 +143,97 @@ def relabel_node(graph, G, atom_names, resnames):
 
         # Create a replacement dictionary for node names
         mapping_atom_name = {node: [atom_names[node], resnames[node]] for node in subgraph.nodes()}
-        
+
         # Create a new subgraph with modified node names
         for node, (atom_name, resname) in mapping_atom_name.items():
-            nx.set_node_attributes(subgraph, {node:node}, "label")
-            nx.set_node_attributes(subgraph, {node:atom_name}, name="atom_name")
-            nx.set_node_attributes(subgraph, {node:resname}, name="resnames")
+            nx.set_node_attributes(subgraph, {node: node}, "label")
+            nx.set_node_attributes(subgraph, {node: atom_name}, name="atom_name")
+            nx.set_node_attributes(subgraph, {node: resname}, name="resnames")
         modified_subgraphs.append(subgraph)  # Add the modified subgraph to the list of modified subgraphs
     return modified_subgraphs
 
 
-def get_graph_fingerprint(g):
-    nodes = g.number_of_nodes()
-    edges = g.number_of_edges()
-    atom_names = " ".join(sorted(nx.get_node_attributes(g, "atom_name").values()))
-    resnames = " ".join(sorted(set((nx.get_node_attributes(g, "resnames").values()))))
-    return(nodes, edges, atom_names, resnames)
+def get_graph_fingerprint(graph):
+    """Generate a fingerprint for a given graph.
+
+    This function calculates a fingerprint for a given graph based on its properties, including
+    the number of nodes, the number of edges, and sorted concatenations of atom names and residue names.
+
+    Parameters
+    ----------
+        graph : networkx.classes.graph.Graph
+            The graph for which the fingerprint is to be generated.
+
+    Returns
+    -------
+        tuple:
+            A tuple containing the fingerprint of the graph, which includes the following elements:
+               - Number of nodes in the graph.
+               - Number of edges in the graph.
+               - Concatenation of sorted atom names of all nodes in the graph.
+               - Concatenation of sorted residue names of all nodes in the graph.
+    """
+    nodes = graph.number_of_nodes()
+    edges = graph.number_of_edges()
+    atom_names = " ".join(sorted(nx.get_node_attributes(graph, "atom_name").values()))
+    resnames = " ".join(sorted(set((nx.get_node_attributes(graph, "resnames").values()))))
+    return (nodes, edges, atom_names, resnames)
 
 
-def print_groupby(object_groupby): 
-    for f, g in object_groupby:
-        print(f"graph_fingerprint {f[0]} | {f[1]} | {f[2]} | {f[3]}")
-        for i in list(g): 
-            print("\t", i.nodes()(data=True))
+def print_groupby(object_groupby):
+    """Print the grouped graphs along with their fingerprints.
+
+    This function iterates over the groups created by the input groupby object and prints
+    the fingerprint along with the nodes and their attributes for each graph in the group.
+
+    Parameters
+    ----------
+        object_groupby : itertools.groupby
+            An object generated by the groupby function.
+
+    Returns
+    -------
+        None
+    """
+    for fingerprint, graph in object_groupby:
+        print(f"graph_fingerprint {fingerprint[0]} | {fingerprint[1]} | {fingerprint[2]} | {fingerprint[3]}")
+        for subgraph in list(graph):
+            print("\t", subgraph.nodes()(data=True))
     print("\n")
 
 
-def count_molecule (list_graph): 
-    """_summary_
-    https://stackoverflow.com/questions/46999771/comparing-a-large-number-of-graphs-for-isomorphism
+def count_molecule(graph_list):
+    """Count the occurrence of molecules in a list of graphs based on their fingerprints.
 
+    This function takes a list of graphs and counts the occurrence of each unique molecule
+    based on their fingerprints, which are calculated using the get_graph_fingerprint function.
+    SOURCE : https://stackoverflow.com/questions/46999771/comparing-a-large-number-of-graphs-for-isomorphism
 
-    Args:
-        list_graph (_type_): _description_
+    Parameters
+    ----------
+        graph_list : list
+            A list of graph objects.
 
-    Returns:
-        _type_: _description_
+    Returns
+    -------
+        dict
+            A dictionary where keys are unique molecules (graph objects) and values are
+        their respective occurrence counts in the input list of graphs.
     """
     dict_count = {}
-
-    sorted_graphs = sorted(list_graph, key=get_graph_fingerprint)
+    sorted_graphs = sorted(graph_list, key=get_graph_fingerprint)
     # print_groupby(groupby(sorted_graphs, key=get_graph_fingerprint))
 
-    for fingerprint, graph in groupby(sorted_graphs, key=get_graph_fingerprint): 
-        # fingerprint : (nb_node, nb_edge, atom_name)
+    for fingerprint, graph in groupby(sorted_graphs, key=get_graph_fingerprint):
+        # fingerprint : (nb_node, nb_edge, atom_name, resname)
         # graph : objet itertools that group all graph with the same fingerprint
         similar_graphs = list(graph)  # A list that contain all graph with the same fingerprint
         nb_graph = len(similar_graphs)  # Number of graph for this fingerprint
 
         if nb_graph == 1:  # For this fingerprint, there is only one graph
             dict_count[similar_graphs[0]] = nb_graph
-        else: 
-            if fingerprint[0] == 1:  # For this fingerprint, all the graph only have one node 
+        else:
+            if fingerprint[0] == 1:  # For this fingerprint, all the graph only have one node
                 dict_count[similar_graphs[0]] = nb_graph
             else:
                 dict_count[similar_graphs[0]] = nb_graph
@@ -193,23 +243,45 @@ def count_molecule (list_graph):
 def print_count(count, option=""):
     """Print the count of molecules.
 
-    Args:
-        count (dict): A dictionary containing information about the molecules.
-        option (str): either we want to print the composition of each molecule (with the option "detail") or not
+    Parameters
+    ----------
+        count : dict
+            A dictionary containing information about the molecules.
+        option : str
+            either we want to print the composition of each molecule (with the option "detail") or not
+
+    Returns
+    -------
+        None
     """
     logger.info("Here is the content of this GRO file :")
-    size = 0
-    for i, (key, value) in enumerate(count.items()):
-        if option=="detail":
-            atom_names = " ".join(nx.get_node_attributes(key, "atom_name").values())
-            logger.success(f"\nMolecule {i+1}: \n\t {key.number_of_nodes():,} atoms \n\t Quantity: {value:,} \n\t Composition: {atom_names}")
-        else : 
-            logger.success(f"\nMolecule {i+1}: \n\t {key.number_of_nodes():,} atoms \n\t Quantity: {value:,}")
-        size += value
-    logger.success(f"It containt {size:,} molecules")
+    nb_molecule = 0
+    for index, (graph, occurence) in enumerate(count.items()):
+        if option == "detail":
+            atom_names = " ".join(nx.get_node_attributes(graph, "atom_name").values())
+            logger.success(f"\nMolecule {index+1}: \n\t {graph.number_of_nodes():,} atoms \n\t Quantity: {occurence:,} \n\t Composition: {atom_names}")
+        else:
+            logger.success(f"\nMolecule {index+1}: \n\t {graph.number_of_nodes():,} atoms \n\t Quantity: {occurence:,}")
+        nb_molecule += occurence
+    logger.success(f"It containt {nb_molecule:,} molecules")
 
 
 def print_graph(dict_graph_count):
+    """Display each graph in a dictionary along with its count.
+
+    This function takes a dictionary where the keys are graph objects and the values
+    are their respective counts. It then displays each graph using Matplotlib's pyplot
+    library, with the graph nodes colored green and labeled with their atom names.
+
+    Parameters
+    ----------
+        dict_graph_count : dict)
+            A dictionary where the keys are graph objects and the values are their respective counts.
+
+    Returns
+    -------
+        None
+    """
     for graph in dict_graph_count.keys():
         plt.figure()
         nx.draw(graph, node_color="green", with_labels=True, labels=nx.get_node_attributes(graph, "atom_name"))
@@ -235,10 +307,16 @@ def delete_hydrogen_grofile(filepath):
 def grodecoder_principal(filepath_gro):
     """Excute the main function for analyzing a .gro file.
 
-    Args:
-        filepath_gro (str): Filepath of the .gro file we want to analyzed
+    Parameters
+    ----------
+        filepath_gro : str
+            Filepath of the .gro file we want to analyzed
+
+    Returns
+    -------
+        None
     """
-    logger.info(f"Filename: {filepath_gro} --------") 
+    logger.info(f"Filename: {filepath_gro} --------")
 
     threshold = max(BOND_LENGTH.values())
     logger.success(f"Threshold: {threshold} Angstrom")
@@ -265,14 +343,13 @@ def grodecoder_principal(filepath_gro):
     connexgraph_return, graph_return = convert_atompairs_to_graph(atom_pair, len(file_gro.atoms))
 
     logger.info("Begin relabel_node ...")
-    list_graph = relabel_node(connexgraph_return, graph_return, file_gro.atoms.names, file_gro.resnames)
+    graph_list = relabel_node(connexgraph_return, graph_return, file_gro.atoms.names, file_gro.resnames)
 
     logger.info("Counting molecules version1...")
-    dict_count = count_molecule(list_graph)
+    dict_count = count_molecule(graph_list)
 
     logger.info("Print dictionnary count and graph...")
     print_count(dict_count)
     # print_graph(dict_count)
-    
-    print("[main] ------------")
 
+    print("[main] ------------")
