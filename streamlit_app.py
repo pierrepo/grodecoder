ac749c917e49031d208fb68df4a617335fa254eb
KarinDuong <karine_duong@hotmail.com>
2024-06-06 18:15:58 +0200

    Streamlit app that analyze an uplodeted structure file and print all the logger message in this app

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,11 @@ import time
 
 from loguru import logger
 import streamlit as st
+from pathlib import Path
+import tempfile
+
+import grodecoder as gd
+
 
 class LauncherTime:
     """Class to measure elapsed time.
@@ -19,18 +24,22 @@ class LauncherTime:
     def elapsed(self):
         return f"{time.perf_counter() - self._start:.2f}"
     
+    
 def print_in_log(iterations):
     for i in range(iterations):
         logger.info(f"hello {i}")
         time.sleep(1)
 
+
 if __name__ == "__main__":
     timer = LauncherTime()
 
     logger.remove()
-    logger.add(st.write, format="{time:YYYY-MM-DD HH:mm:ss} | {extra[timer].elapsed} s | {level} | {message}")
-    logger.add("test.log", format="{time:YYYY-MM-DD HH:mm:ss} | {extra[timer].elapsed} s | {level} | {message}")
-    logger = logger.bind(timer=timer)
+    # logger.add(st.write, format="{time:YYYY-MM-DD HH:mm:ss} | {extra[timer].elapsed} s | {level} | {message}")
+    # logger.add("test.log", format="{time:YYYY-MM-DD HH:mm:ss} | {extra[timer].elapsed} s | {level} | {message}")
+    logger.add(st.write, format="{time:YYYY-MM-DD HH:mm:ss} | {level} | {message}")
+    logger.add("test.log", format="{time:YYYY-MM-DD HH:mm:ss} | {level} | {message}")
+    # logger = logger.bind(timer=timer)
     st.markdown(
         """
         <style>
@@ -41,10 +50,27 @@ if __name__ == "__main__":
         """, unsafe_allow_html=True)
     st.markdown("# Simple app")
     
-    
-    if st.button("Run new analysis"):
-        timer.reset()
+
+    uploaded_file = st.file_uploader("Choose a structural file", type=["pdb", "gro"])
+    if (uploaded_file is not None) and st.button("Run new analysis") : 
+        temp_dir = tempfile.mkdtemp()
+        path = Path(temp_dir) / uploaded_file.name
+        with open(path, "wb") as f:
+            f.write(uploaded_file.getvalue())
+        st.write(f"Saved to {path}")
         with st.spinner("Running analysis..."):
-            print_in_log(5)
-        st.write("Done!")
- 
+            gd.main(
+                path,
+                draw_graph_option=False,
+                check_overlapping_residue=False,
+                check_connectivity=False,
+                bond_threshold="auto",
+                query_pdb=True,
+            )
+
+    
+    # if st.button("Run new analysis"):
+    #     timer.reset()
+    #     with st.spinner("Running analysis..."):
+    #         print_in_log(5)
+    #     st.write("Done!")
