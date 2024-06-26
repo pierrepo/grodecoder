860c415357aba99fc03406ceb82128083389299d
KarinDuong <karine_duong@hotmail.com>
2024-06-26 20:11:05 +0200

    Use the return object (the JSON filepath) given by the main, instead of fetch the last line of the log file

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,18 +9,6 @@ import streamlit as st
 import grodecoder as gd
 
 
-def last_line_log_file():
-    with open("test.log", "rb") as file:
-        try:
-            file.seek(-2, os.SEEK_END)
-            while file.read(1) != b"\n":
-                file.seek(-2, os.SEEK_CUR)
-        except OSError:
-            file.seek(0)
-        last_line = file.readline().decode()
-    return last_line
-
-
 if __name__ == "__main__":
     logger.remove()
     logger.add(st.write, format="{time:YYYY-MM-DD HH:mm:ss} | {message}", level="INFO")
@@ -62,9 +50,6 @@ if __name__ == "__main__":
     )
 
     st.write("Options:")
-    check_overlapping_residue_value = st.checkbox(
-        "Check overlapping residue", value=False
-    )
     check_connectivity_value = st.checkbox(
         "Check connectivity (add degree and number of edge in the fingerprint) ",
         value=False,
@@ -80,27 +65,19 @@ if __name__ == "__main__":
             f.write(uploaded_file.getvalue())
         st.write(f"Saved to {path}")
         with st.spinner("Running analysis..."):
-            gd.main(
+            JSON_filepath = gd.main(
                 path,
-                check_overlapping_residue=check_overlapping_residue_value,
                 check_connectivity=check_connectivity_value,
                 bond_threshold=bond_threshold_value,
                 query_pdb=True,
             )
 
-        # Fetch the last line of the log file
-        last_line = last_line_log_file()
-        last_line_filename = last_line.split()[-1]
-
-        # Test if the last line of the log file is a JSON
-        # That also mean that the analysis terminated correctly
-        if last_line_filename.split(".")[-1] == "json":
-            with open(last_line_filename, "r") as file:
-                file_content = file.read()
-            st.toast("Analysis completed successfully 🎉", icon="🥳")
-            st.download_button(
-                label="Download molecular inventory",
-                data=file_content,
-                file_name=last_line_filename,
-                mime="text/plain",
-            )
+        with open(JSON_filepath, "r") as file:
+            file_content = file.read()
+        st.toast("Analysis completed successfully 🎉", icon="🥳")
+        st.download_button(
+            label="Download molecular inventory",
+            data=file_content,
+            file_name=JSON_filepath,
+            mime="text/plain",
+        )
