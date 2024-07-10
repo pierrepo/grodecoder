0a6481623a6886599d72e6fed3fd9b9e219d26ba
KarinDuong <karine_duong@hotmail.com>
2024-07-10 02:12:13 +0200

    Update the README with the composition of the new added files, change the condition in 'is_nucleic_acid' to return true even if the length of the resname from the graph is over 1

--- mol_def.py
+++ mol_def.py
@@ -42,9 +42,11 @@ AMINO_ACID_DICT["CYSP"] = "C"
 # (mostly AMBER and CHARMM, the other don't have these files) 
 # in https://github.com/gromacs/gromacs/blob/main/share/top/)
 NUCLEIC_ACIDS_DNA = {"DA": 'A',
-                     "DT": 'T', 
-                     "DC": 'C', 
-                     "DG": 'G', 
+                     "DT": 'T',
+                     "DC": 'C',
+                     "DG": 'G',
+                     "C": 'C',
+                     "G": 'G',
 
                      "DA5": 'A',
                      "DA3": 'A',
@@ -56,10 +58,12 @@ NUCLEIC_ACIDS_DNA = {"DA": 'A',
                      "DG3": 'G', 
                     }
 
-NUCLEIC_ACIDS_RNA ={"RA": 'A', 
-                    "RU": 'U', 
-                    "RC": 'C', 
-                    "RG": 'G', 
+NUCLEIC_ACIDS_RNA ={"RA": 'A',
+                    "RU": 'U',
+                    "RC": 'C',
+                    "RG": 'G',
+                    "C": 'C',
+                    "G": 'G',
 
                     "RA5": 'A', 
                     "RA3": 'A', 
