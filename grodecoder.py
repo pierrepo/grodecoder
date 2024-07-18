85a7a6527fee09c8797e75e9dbc4e1719844c297
KarinDuong <karine_duong@hotmail.com>
2024-07-18 20:58:02 +0200

    Add new file, that containt nucleic acids and update the README with their composition

--- grodecoder.py
+++ grodecoder.py
@@ -1456,7 +1456,7 @@ def is_nucleic_acid(graph: nx.classes.graph.Graph) -> bool:
         bool
             True if the molecule is a nucleic acid, False otherwise.
     """
-    set_key_amino_acid_mda = set(mol_def.NUCLEIC_ACIDS.keys())
+    set_key_amino_acid_resname = set(mol_def.NUCLEIC_ACIDS.keys())
     set_res_name_graph = set(nx.get_node_attributes(graph, "residue_name").values())
     return len(set_key_amino_acid_resname.intersection(set_res_name_graph)) > 1
 
