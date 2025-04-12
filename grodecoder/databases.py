69555de2284bb44a00d10b09ba62c62dc155f194
benoistlaurent <benoist.laurent@cnrs.fr>
2025-04-12 12:51:46 +0200

    refactoring database

--- grodecoder/databases.py
+++ grodecoder/databases.py
@@ -1,15 +1,18 @@
+from __future__ import annotations
 import json
 from dataclasses import dataclass
 from pathlib import Path
+from typing import Iterable
 
 from ._typing import PathLike, Json
 
 
-MOLECULE_DEFINITIONS_DATABASE_PATH = Path(__file__).parent / "data" / "databases" / "molecule_definitions.json"
+MOLECULE_DEFINITIONS_DATABASE_PATH = (
+    Path(__file__).parent / "data" / "databases" / "molecule_definitions.json"
+)
 assert MOLECULE_DEFINITIONS_DATABASE_PATH.is_file()
 
 
-# TODO: convert to pydantic
 @dataclass
 class SmallMoleculeDefinition:
     """Base class for a small molecule as represented in grodecoder molecule definition database.
@@ -28,6 +31,15 @@ class SmallMoleculeDefinition:
     name: str
     residue_name: str
     atom_names: list[str]
+
+    def __hash__(self):
+        return hash((self.name, self.residue_name, tuple(self.atom_names)))
+
+    def almost_eq(self, other) -> bool:
+        """Returns True if `residue_name` and `atom_names` members are equal."""
+        if not isinstance(other, SmallMoleculeDefinition):
+            return False
+        return self.residue_name == other.residue_name and set(self.atom_names) == set(other.atom_names)
 
 
 class IonDefinition(SmallMoleculeDefinition):
@@ -57,6 +69,16 @@ class SolventDefinition(SmallMoleculeDefinition):
     """
 
 
+class DuplicateDefinitionError(Exception):
+    """Raised when the same molecule is defined multiple times in the database."""
+
+    def __init__(self, SmallMoleculeDefinition: SmallMoleculeDefinition):
+        self.definition = SmallMoleculeDefinition
+
+    def __str__(self):
+        return f"Duplicate definition found for {self.definition.residue_name} with atom names {self.definition.atom_names}"
+
+
 def read_molecule_database(path: PathLike) -> dict[str, Json]:
     """Reads the grodecoder molecule database.
 
@@ -71,23 +93,40 @@ def read_molecule_database(path: PathLike) -> dict[str, Json]:
         The values depend on the type of molecule.
     """
     with open(path, "r") as f:
-        molecule_database = json.load(f)
+        source_database = json.load(f)
+
+    # Safety checks and reformat.
+    molecule_database = source_database.copy()
+
+    for molecule_type in ("ions", "solvents"):
+        molecule_database[molecule_type] = {}
+
+        for molecule in source_database[molecule_type]:
+            residue_name = molecule["residue_name"]
+            molecule_database[molecule_type].setdefault(residue_name, [])
+
+            # Check if the molecule is already defined.
+            current_molecule = SmallMoleculeDefinition(**molecule)
+            for existing_molecule in molecule_database[molecule_type][residue_name]:
+                if current_molecule.almost_eq(existing_molecule):
+                    raise DuplicateDefinitionError(current_molecule)
+
+            # Add the entry to the database.
+            molecule_database[molecule_type][residue_name].append(
+                current_molecule)
+
     return molecule_database
 
 
 MOLECULE_DEFINITIONS = None
 if MOLECULE_DEFINITIONS is None:
-    MOLECULE_DEFINITIONS = read_molecule_database(MOLECULE_DEFINITIONS_DATABASE_PATH)
+    MOLECULE_DEFINITIONS = read_molecule_database(
+        MOLECULE_DEFINITIONS_DATABASE_PATH)
 
 
-def get_ion_definitions() -> list[IonDefinition]:
-    # Convert to `IonDefinition`.
-    # TODO: automate this using pydantic
-    return [IonDefinition(**molecule) for molecule in MOLECULE_DEFINITIONS["ions"]]
+def get_ion_definitions() -> list[SmallMoleculeDefinition]:
+    return MOLECULE_DEFINITIONS["ions"]
 
 
-def get_solvent_definitions() -> list[SolventDefinition]:
-    # TODO: automate this using pydantic
-    return [SolventDefinition(**molecule) for molecule in MOLECULE_DEFINITIONS["solvents"]]
-
-
+def get_solvent_definitions() -> list[SmallMoleculeDefinition]:
+    return MOLECULE_DEFINITIONS["solvents"]
--- v2.py
+++ v2.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from loguru import logger
 
 import grodecoder as gd
 import grodecoder.v1 as v1
@@ -6,8 +7,10 @@ import grodecoder.databases as DB
 
 # DEBUG
 import icecream
+import time
 
 icecream.install()
+
 
 # Import the "old" grodecoder for comparison.
 # To be removed.
@@ -18,8 +21,8 @@ v1.logger.remove()
 class SmallMoleculeCount:
     name: str
     residue_name: str
-    number_of_atoms: int
-    number_of_residues: int
+    number_of_atoms: int = 0
+    number_of_residues: int = 0
 
 
 def count_small_molecule(
@@ -42,45 +45,71 @@ def count_ions(universe: gd.UniverseLike) -> list[SmallMoleculeCount]:
     counts = [count_small_molecule(universe, ion) for ion in ions]
     return [count for count in counts if count.number_of_atoms > 0]
 
+
 def count_solvents(universe: gd.UniverseLike) -> list[SmallMoleculeCount]:
     solvents = DB.get_solvent_definitions()
     counts = [count_small_molecule(universe, solvent) for solvent in solvents]
     return [count for count in counts if count.number_of_atoms > 0]
 
 
+class NoDefinitionFoundError(Exception):
+    """Base class for all definition not found errors."""
 
-# def count_ions_solvents_grodecoder(universe: UniverseLike) -> dict[str, int]:
-#     warnings.filterwarnings("ignore")
-#     _, counts = v1.count_remove_ion_solvant(universe, "foo")
-#     result = {}
-#     for molecule, dict_count in counts.items():
-#         count = dict_count.get("graph")
-#         res_name = " ".join(
-#             set(nx.get_node_attributes(molecule, "residue_name").values())
-#         )
-#         result[res_name] = count
-#     return result
+    def __init__(self, residue_name: str, number_of_atoms: int):
+        self.residue_name = residue_name
+        self.number_of_atoms = number_of_atoms
 
 
-# def create_inventory(ion_count: dict[str, SmallMoleculeCount], solvent_count: dict[str, SmallMoleculeCount]):
-#     inventory = []
-#     for count in ion_count.values():
-#         item = {
-#             "id": len(inventory) + 1,
-#             "number_of_atoms": count.number_of_atoms,
-#             "number_of_molecules": count.number_of_residues,
-#             "residue_names": count.residue_name,
-#         }
-#         inventory.append(item)
-#
-#     for residue_name, count in solvent_count.items():
-#         item = {
-#             "id": len(inventory) + 1,
-#             "number_of_atoms": count.number_of_atoms,
-#             "number_of_molecules": count.number_of_residues,
-#             "residue_names": count.residue_name,
-#         }
-#         inventory.append(item)
+class NoDefinitionWithMatchingNumberOfAtoms(NoDefinitionFoundError):
+    """Exception raised when no definition is found for a given residue name."""
+
+    def __str__(self):
+        return f"No definition found for residue '{self.residue_name}' with {self.number_of_atoms} atoms"
+
+
+class NoDefinitionWithMatchingAtomNames(NoDefinitionFoundError):
+    """Exception raised when no definition is found for a given residue name."""
+
+    def __str__(self):
+        return f"No definition found for residue '{self.residue_name}' with atom names {self.atom_names}"
+
+
+class MultipleDefinitionsWithMatchingAtomNames(NoDefinitionFoundError):
+    """Exception raised when multiple definitions are found for a given residue name."""
+
+    def __str__(self):
+        return (
+            f"Multiple definitions found for residue '{self.residue_name}' with atom names {self.atom_names}"
+        )
+
+
+def count2(universe) -> SmallMoleculeCount:
+    solvents = DB.get_solvent_definitions()
+    counts = {}
+
+    for residue_name, definitions in solvents.items():
+        selection = universe.select_atoms(f"resname {residue_name}")
+
+        for residue in selection.residues:
+            # Definitions with matching residue name and number of atoms.
+            matches = [d for d in definitions if len(residue.atoms) == len(d.atom_names)]
+            if not matches:
+                raise NoDefinitionWithMatchingNumberOfAtoms(residue_name, len(residue.atoms))
+
+            # Look for a definition with the same atom names.
+            match = None
+            for definition in matches:
+                if set(definition.atom_names) == set(residue.atoms.names):
+                    match = definition
+                    break
+
+            if match is None:
+                raise NoDefinitionWithMatchingAtomNames(residue_name, residue.atoms.names)
+
+            counts.setdefault(match, SmallMoleculeCount(definition.name, residue_name))
+            counts[match].number_of_atoms += len(residue.atoms)
+            counts[match].number_of_residues += 1
+    return list(counts.values())
 
 
 def main():
@@ -88,13 +117,13 @@ def main():
     universe = gd.read_topology(topology_path)
 
     water = universe.select_atoms("resname TIP3")
-    print(len(water))
-    print(len(water.residues))
+    expected_number_of_atoms = len(water)
+    expected_number_of_residues = len(water.residues)
 
-    ion_counts = count_solvents(universe)
-
-    for i in ion_counts:
-        ic(i)
+    start = time.perf_counter()
+    result = count2(universe)
+    print(f"method 2: {time.perf_counter() - start:.2f} seconds")
+    ic(result)
 
 
 if __name__ == "__main__":
